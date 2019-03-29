4769d089deb6f22cd2ed59ed34d36e82dce4db75
chema <chemabetis1208@gmail.com>
2019-03-29 08:57:11 +0100

    Modificación Modelo

--- MVC/src/es/studium/Contador/ModeloContador.java
+++ MVC/src/es/studium/Contador/ModeloContador.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class ModeloContador {
 
-	public int contarPalabras(String cadena) {
+	public Integer contarPalabras(String cadena) {
 		StringTokenizer st = new StringTokenizer(cadena);
 		Integer palabras = (st.countTokens());
 		if(cadena.equals("")) {
